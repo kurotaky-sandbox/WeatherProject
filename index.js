d8c3bfb054b9e732ee4a43a86a3d64cf816eea90
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2018-01-24 09:28:21 +0900

    WeatherProject

---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import WeatherProject from "./WeatherProject";
+import OpenWeatherMap from "./open_weather_map";
+
+AppRegistry.registerComponent('WeatherProject', () => WeatherProject);
